aa730809cc31fa52d84bb2fc15462b8bb20cb717
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-02-22 22:40:36 +0600

    setup MEDIA_ROOT

--- gambo/settings.py
+++ gambo/settings.py
@@ -104,7 +104,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Stripe
 AUTH_USER_MODEL = "users.User"
--- gambo/urls.py
+++ gambo/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('super-admin/', admin.site.urls),
@@ -13,3 +15,6 @@ urlpatterns = [
     path('offers/', include('offers.urls')),
     path("stripe/", include("djstripe.urls", namespace="djstripe")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
