42b284b5a60e6e2137e2b57d4b85f63e13e6951c
mohammadgholampour <m.gholampour2002@gmail.com>
2025-09-04 22:22:21 +0330

    test 1

--- gambo/settings.py
+++ gambo/settings.py
@@ -73,11 +73,11 @@ WSGI_APPLICATION = "gambo.wsgi.application"
 
 # --- Database (PostgreSQL on Render) ---
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")  # Render provides this automatically
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
-
 # --- Password Validation ---
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
