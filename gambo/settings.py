006284b7fc1791280ce27b3f281be835c1670938
mohammadgholampour <m.gholampour2002@gmail.com>
2025-09-04 22:33:23 +0330

    fix data base

--- gambo/settings.py
+++ gambo/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = "gambo.wsgi.application"
 # --- Database (PostgreSQL on Render) ---
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "PORT": os.environ.get("DATABASE_PORT", 5432),
     }
 }
 # --- Password Validation ---
