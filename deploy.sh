eca508ca4a3ed376c454b7e92baaced9d9b09d5a
mohammadgholampour <m.gholampour2002@gmail.com>
2025-09-05 00:51:56 +0330

    test 11

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,19 @@
 # Activate virtual environment
 source "$VIRTUAL_ENV/bin/activate"
 
+# Upgrade pip
+pip install --upgrade pip
+
 # Install requirements
 pip install -r requirements.txt
 
 # Make migrations and migrate
 python manage.py makemigrations
+# Migrate
 python manage.py migrate --noinput
 
 # Create default SiteSettings AFTER migrations
+# This is a workaround to create a default SiteSettings after migrations
 python manage.py shell -c "from core.models import SiteSettings; SiteSettings.objects.get_or_create(pk=1)"
 
 # Collect static files
