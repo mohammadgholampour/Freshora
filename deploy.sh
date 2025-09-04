44ff642cce9bd73a1d129add9ef1f8c3de2f69b5
mohammadgholampour <m.gholampour2002@gmail.com>
2025-09-05 00:02:51 +0330

    test 5

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ source "$VIRTUAL_ENV/bin/activate"
 # Install requirements
 pip install -r requirements.txt
 
-# Make migrations
+# Make migrations and migrate
 python manage.py makemigrations
 python manage.py migrate --noinput
 
